919b3d1b77c6ba4a0022ef6590a3a8467c0335eb
Cameron Daigle <cameron@hashrocket.com>
2011-05-09 20:41:02 -0400

    Refactor

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,91 +1,69 @@
 $(function() {
 
   var $cv = $("#conveyor");
-  var $items = $("#items");
+  var $row = $("#items");
+  var $items = $row.children();
   var $puncher = $("#puncher");
   var $panel = $("#panel");
   var $cutout = $("#cutout");
   var $glow = $("#glow");
 
-  populate();
+  walk();
 
-  setInterval(function() {
+  function walk() {
     populate();
-    var $c = $items.children();
-    var $check = $("<span />", {id: "check"}).appendTo($("body"));
-    if ($("body").find("#check").length) {
-      $c.animate({
-        right: "-=132px",
-        bottom: "-=76px"
-      }, {
-        queue: false,
-        duration: 200,
-        complete: function() {
-          if ($c.index() == 0) {
-            $c.last().removeClass().css({
-              right: 458,
-              bottom: 684
-            }).insertBefore($c.eq(0));
-            placePanel();
-            punchPanel();
-          }
+    $cv.stop(true, false);
+    $items.animate({
+      right: "-=132px",
+      bottom: "-=76px"
+    }, 200, function(e) {
+        if ($cv.find(":animated").length == 1) {
+          placePanel();
+          punchPanel();
         }
-      });
-      $("#check").detach();
-    };
-  }, 2000);
+    });
+  };
 
   function populate() {
-    var $original = $items.children().detach().first().css({
-      right: -730,
-      bottom: 0
+    var $original = $items.css({
+      right: 590,
+      bottom: 760
     });
-    for (var i = 0, max = 9; i <= max; i++) {
-      var $n = $original.clone().prependTo($items).css({
-        right: "+=" + 132 * i + "px",
-        bottom: "+=" + 76 * i + "px"
-      });
-      if( i < 5 ) {
-        $n.addClass("filled");
-      } else if ( i < 7 ) {
-        $n.addClass("punched");
+    $items.removeClass().each(function(i) {
+      var $c = $(this);
+      if ($c.index() > 0) {
+        $c.css({
+          right: "-=" + 132 * i + "px",
+          bottom: "-=" + 76 * i + "px"
+        });
       }
-    }
+      if (i > 5) {
+        $c.addClass("filled");
+      } else if (i > 3) {
+        $c.addClass("punched");
+      }
+    });
   }
 
   function placePanel() {
     $glow.delay(600).fadeIn(600, function() {
-      $panel.animate({ top: 206 }, {
-        queue: false,
-        duration: 100,
-        complete: function() {
+      $panel.animate({ top: 206 }, 100, function() {
         $panel.css({ top: 94 });
-        $items.find(":eq(5)").addClass("filled");
-        $glow.fadeOut(200);
-        }
+        $row.find(":eq(5)").addClass("filled");
+        $glow.fadeOut(400, walk);
       });
     });
   }
 
   function punchPanel() {
-    $puncher.animate({ top: -60 }, {
-      duration: 400,
-      queue: false, 
-      complete: function() {
+    $puncher.animate({ top: -60 }, 400, function() {
       $cutout.show().css("top", 80);
-      $puncher.animate({ top: 0 }, {
-        duration: 50,
-        queue: false,
-        complete: function() {
-          $items.animate({ paddingTop: 10 }, 50).animate({ paddingTop: 0 }, 50);
-          $items.find(":eq(3)").addClass("punched");
-          $cutout.animate({
-            top: $(window).height() + 200
-          }, { duration: 500, queue: false });
+      $puncher.animate({ top: 0 }, 50, function() {
+          $row.animate({ paddingTop: 10 }, 50).animate({ paddingTop: 0 }, 50);
+          $row.find(":eq(3)").addClass("punched");
+          $cutout.animate({ top: $(window).height() + 200 }, 500);
           $puncher.delay(50).animate({ top: -100 }, 200);
-        }
       });
-      }
     });
   }
 
