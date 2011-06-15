a9bed299b31fa0669e142146f1baacae6fa426d3
Cameron Daigle <cameron@hashrocket.com>
2011-06-15 12:34:56 -0400

    Credit in js and css files

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,9 @@
+/*
+  http://fabricationgem.org
+  by Cameron Daigle http://camerondaigle.com
+  for Hashrocket Ventures http://hashrocket.com
+*/
+
 $(function() {
 
   if (!(/^Win/.test(window.navigator.platform))) {
