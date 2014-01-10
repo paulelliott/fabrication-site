bfceea7860915ebf02ad5d504824c34066aaed5c
Cameron Daigle <camerondaigle@gmail.com>
2014-01-10 14:10:46 -0500

    Enable syntax highlighting (content not updated yet)

--- config.rb
+++ config.rb
@@ -2,6 +2,11 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
+activate :syntax
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
