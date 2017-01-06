322b0b8b912396bb1398d70cb52a42c537d69c19
Paul Elliott <paul@codingfrontier.com>
2017-01-06 09:10:55 -0500

    Reorganize config.rb

--- config.rb
+++ config.rb
@@ -9,25 +9,25 @@ set :images_dir, 'images'
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
 
-activate :asset_hash
 activate :dotenv
 activate :syntax
-
-activate :google_analytics do |ga|
-  ga.tracking_id = 'UA-4408103-7'
-end
-
-activate :s3_sync do |s3_sync|
-  s3_sync.after_build = true
-end
 
 configure :development do
   activate :livereload
 end
 
+activate :google_analytics do |ga|
+  ga.tracking_id = 'UA-4408103-7'
+end
+
 configure :build do
+  activate :asset_hash
   activate :minify_css
   activate :minify_javascript
+end
+
+activate :s3_sync do |s3_sync|
+  s3_sync.after_build = true
 end
 
 activate :cloudfront do |cf|
